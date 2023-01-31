7005ba035c2d729070d44020a6fdf0e791259c6a
Guilherme Henrique <guiroox2@gmail.com>
2023-01-31 11:43:26 -0300

    change background based on media queries

--- assets/components/script.js
+++ assets/components/script.js
@@ -2,11 +2,11 @@ function toggleMode() {
   const html = document.documentElement;
 
   //  Cria uma condição para adicionar ou remover o modo de visualização do tema Dark/Light ao clicar no botão.
-  if (html.classList.contains("light")) {
-    html.classList.remove("light");
-  } else {
-    html.classList.add("light");
-  }
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light");
+  // } else {
+  //   html.classList.add("light");
+  // }
 
   // O metodo toggle faz a mesma coisa do If/Else, porem faz essa alteração por de baixo dos panos.
   html.classList.toggle("light");
@@ -14,7 +14,7 @@ function toggleMode() {
   // Pega a tag imag para troca de imagem
   const img = document.querySelector("#profile img");
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "assets/image/avatar-lightmode.png");
+    // img.setAttribute("src", "assets/image/avatar-lightmode.png");
   } else {
     img.setAttribute("src", "assets/image/avatar.png");
   }
